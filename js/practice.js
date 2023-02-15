bd7a3b97a64194b6f62d663d037ce9b1cf6de1da
Marius CA <marius@example.com>
2023-02-15 16:41:01 +0200

    colorFullLiNodeList

--- js/practice.js
+++ js/practice.js
@@ -35,3 +35,40 @@ const dateInLtFormat = now.toLocaleString('lt-LT', { dateStyle: 'full' });
 console.log('now ===', dateInLtFormat);
 
 pDate.textContent = dateInLtFormat;
+
+// 6. pakeisti paskutinio <ul class="colors"> elemento spalva i tomato
+
+// nusitaikyti i sarasa
+const colorsListEl = document.querySelector('.colors');
+console.log('colorsListEl ===', colorsListEl);
+// pasiimti jo paskutini elementa ir paspalvinti
+colorsListEl.lastElementChild.style.color = 'tomato';
+
+// 7. <p id="art__text">Lorem ipsum dolor</p> elemento texta padaryti didziosiomis raidemis, ne su css.
+
+// nusitaikyti,
+// pasiimti teksta
+let elText = pEl.textContent;
+console.log('elText ===', elText);
+// textui padaryti toUpperCase
+const textUpper = elText.toUpperCase();
+console.log('textUpper ===', textUpper);
+// irasyti atgal i elementa
+pEl.textContent = textUpper;
+
+// 8. prideti kiekvienam colors saraso elementui texta ' added'
+
+// psiaudo kodas
+
+// 1. nusitaikom i visus colors li - colekcija
+const colorFullLiNodeList = document.querySelectorAll('.colors .colLi');
+console.log('colorFullLiNodeList ===', colorFullLiNodeList);
+// 2. sukam cikla
+for (let liEl of colorFullLiNodeList) {
+  // let text = liEl.textContent;
+  // let textWithAdded = text + ' added';
+  // 3. ciklo metu mes prie esamos li texto vertes pridedam ' added'
+  // liEl.textContent = textWithAdded;
+  // liEl.textContent = liEl.textContent + ' added';
+  // liEl.textContent += ' added';
+}
