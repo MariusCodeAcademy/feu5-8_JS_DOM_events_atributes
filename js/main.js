0643eeae6fb61041bac5e869742ccdcaaecf6aaf
Marius CA <marius@example.com>
2023-02-15 10:48:02 +0200

    get by class name

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ console.log('main.js file was loaded');
 
 // nusitaikau
 const appContainer = document.getElementById('app');
+// pasitikrinimas ar pataikem su nusitaikymu
+if (appContainer === null) {
+  console.warn('nera appContainer');
+}
+
+// console.log('appContainer ===', appContainer);
 
 // sukurti funckija createTitle()
 // sukuria ir patalpina appContaineryje h2 su textu 'I am new title'
@@ -12,4 +18,28 @@ function createTitle() {
   h2El.textContent = 'I am new title';
   appContainer.append(h2El);
 }
-createTitle();
+// createTitle();
+
+// document.getElementsByClassName('klasesPavadinimas')
+// grazina kolekcija eleentu su argumente nurodytu pavadinimu
+const listEl = document.getElementsByClassName('list');
+
+listEl[0].style.background = 'tomato';
+
+console.log('listEl ===', listEl);
+
+// rasti visus elementus su klase listEl
+const allLis = document.getElementsByClassName('listEl');
+console.log('allLis ===', allLis);
+
+// gaunam HMTLCollecion kurios el galim pasiekti per index
+// nudazyti vidurion el fona
+
+// allLis[0].style.background = 'blue';
+// allLis[1].style.background = 'blue';
+// allLis[2].style.background = 'blue';
+
+for (let i = 0; i < allLis.length; i++) {
+  const oneLiEl = allLis[i];
+  oneLiEl.style.background = 'green';
+}
