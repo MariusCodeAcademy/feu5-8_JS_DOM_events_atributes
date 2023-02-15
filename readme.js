d8b1a0633a340d67c54b832de7d4b83409686ced
Marius CA <marius@example.com>
2023-02-15 15:01:21 +0200

    fix

--- readme.js
+++ readme.js
@@ -12,7 +12,7 @@ susikuriam praktice.js
 
 5. nusitaikyti ir pakeisti teksta <p id="art__date">Date</p> i siandienos data
 
-6. pakeisti paskutinio <ul id="colors" class="list"> elemento spalva i tomato
+6. pakeisti paskutinio <ul class="colors"> elemento spalva i tomato
 
 7. <p id="art__text">Lorem ipsum dolor</p> elemento texta padaryti didziosiomis raidemis, ne su css.
    7.1 <p id="art__text">Lorem ipsum dolor</p> elemento texta surasyti is priesingos puses
@@ -22,5 +22,4 @@ susikuriam praktice.js
 9. (uzsikomentuoti [7] uzd vygdyma) padaryti kad kiekvieno colors saraso li elemento fono spalva butu tokia kaip parasyta li texte
 
 10. paimti visas colors elemento spalvas is li teksto, surasyti i viena stringa ir iterpti kaip paskutini li elementa su visu spalvu sarasu atskirtu kableliais
-
 */
