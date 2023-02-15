f05ba615569d526e83f6906a27c64580d1b36e17
Marius CA <marius@example.com>
2023-02-15 10:24:56 +0200

    html sample

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 'use strict';
 console.log('main.js file was loaded');
+
+// nusitaikau
+const appContainer = document.getElementById('app');
+
+// sukurti funckija createTitle()
+// sukuria ir patalpina appContaineryje h2 su textu 'I am new title'
+
+function createTitle() {
+  const h2El = document.createElement('h2');
+  h2El.textContent = 'I am new title';
+  appContainer.append(h2El);
+}
+createTitle();
